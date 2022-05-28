a9f99187d9d0e58c5ce4d59eedc5376b984c29b0
JoelB09 <joel.bahati@flemingcollege.ca>
2022-05-28 19:12:27 -0400

    Revert "modified both files"
    This reverts commit 0f4f41db5e4421038fec6955648b2b6579f82b1f.

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 #print lines from zen of python 
 print("There should be one-- and preferably only one --obvious way to do it.")
 
+print("Although that way may not be obvious at first unless you're Dutch.")
 
--- script2.py
+++ script2.py
@@ -1,4 +1,3 @@
-#print lines from zen of python
 print("Simple is better than complex.")
 
 print("Complex is better than complicated.")
